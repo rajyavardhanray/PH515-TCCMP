12c55960dfa8e51996e5a10a0583dc3941ce994f
R Ray <45084070+rajyavardhanray@users.noreply.github.com>
2023-01-23 00:26:19 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PH515'
+copyright = '2023, Ray'
+author = 'Ray'
 
 release = '0.1'
 version = '0.1.0'
